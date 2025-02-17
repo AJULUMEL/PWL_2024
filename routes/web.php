d37c9df89367221163fe637e000adc1977a2cdfb
AJULUMEL <azruljoss@gmail.com>
2025-02-17 12:58:27 +0700

    Refactor web routes to use dedicated controllers for routing

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,31 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
+| 
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/hello', function ()
-{    return 'Hello World'; }); 
+Route::get('/hello', [WelcomeController::class,'hello']); 
 
 Route::get('/world', function () {    return 'World'; 
 }); 
 
 Route::get('/selamat', function (){return 'Selamat Datang';});
 
-Route::get('/about', function (){return 'Dandi Azrul S - 2341720118';});
 
 Route::get( '/user/{name} ', action: function ($name) { return 'Nama Saya '.$name; }); 
 
@@ -29,13 +33,16 @@ Route::get('/posts/{post}/comments/{comment}', function
 ($postId, $commentId) { 
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;});
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' . $id;});
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('photos', PhotoController::class);
 
 Route::get('/user/{name?}', function ($name='Ajul') {return 'Nama Saya '.$name;});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 
