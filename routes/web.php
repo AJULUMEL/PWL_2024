0cb68e03c676f7719e416c02662905f221f77b05
AJULUMEL <azruljoss@gmail.com>
2025-02-17 12:03:09 +0700

    Add basic Laravel routes with various path and parameter examples

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function ()
+{    return 'Hello World'; }); 
+
+Route::get('/world', function () {    return 'World'; 
+}); 
+
+Route::get('/selamat', function (){return 'Selamat Datang';});
+
+Route::get('/about', function (){return 'Dandi Azrul S - 2341720118';});
+
+Route::get( '/user/{name} ', action: function ($name) { return 'Nama Saya '.$name; }); 
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;});
+
+Route::get('/user/{name?}', function ($name='Ajul') {return 'Nama Saya '.$name;});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
